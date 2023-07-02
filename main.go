1aecd5f3992b91220831282f0c86e588f8ffcb18
notnulldev <notnulldev0@gmail.com>
2023-07-02 03:03:16 +0200

    first commit

---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"html/template"
+	"os"
+	"path/filepath"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/template/html/v2"
+)
+
+func main() {
+	engine := html.New("./views", ".html")
+	engine.Reload(true)
+
+	engine.AddFunc("getCssAsset", func(name string) (res template.HTML) {
+		filepath.Walk("public/assets", func(path string, info os.FileInfo, err error) error {
+			if err != nil {
+				return err
+			}
+			if info.Name() == name {
+				res = template.HTML("<link rel=\"stylesheet\" href=\"/" + path + "\">")
+			}
+			return nil
+		})
+		return
+	})
+
+	app := fiber.New(fiber.Config{
+		Views:       engine,
+		ViewsLayout: "layouts/main",
+	})
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.Render("main", fiber.Map{
+			"Title": "Hello, World!",
+		})
+	})
+
+	app.Static("/public", "./public")
+
+	if err := app.Listen(":4444"); err != nil {
+		panic(err.Error())
+	}
+}
---
+++ readme.md
@@ -0,0 +1,8 @@
+```bash
+# to start tailwind in watch mode
+npx tailwindcss -i ./app.css -o ./public/assets/app.css --watch
+# to start dev server
+go run .
+```
+
+Make a changes to the template and then reload the page to see the changes.
